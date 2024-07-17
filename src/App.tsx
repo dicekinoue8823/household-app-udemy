a5fdd27c42a9d9273e435b3e5b5052665d9fe6f0
dicek8823 <dicek8823@icloud.com>
2024-07-17 17:15:31 +0900

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+import '@/App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "@/pages/Home";
+import Report from "@/pages/Report";
+import NoMatch from "@/pages/NoMatch";
+
+function App()  {
+    return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
+    );
+}
+
+export default App
